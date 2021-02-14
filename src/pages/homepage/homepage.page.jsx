3bec5e60c5be31895e2085a1b05e949b9fe983db
jonathan <yonibrese@gmail.com>
2021-02-14 15:16:49 +0200

    added firebase auth, sign in whith google, and a sign out function: connected to the header component

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,6 +1,6 @@
 import './homepage.style.scss';
 
-const Homepage = () => {
+const Homepage = (user) => {
     return(
         <div className="home">
             <h1>This is the WolfMath ReactJS boiler plate!</h1>
