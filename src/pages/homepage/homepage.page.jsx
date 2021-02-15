0c6203799efc8ffa5fb529cce90de37b0c1ab5cc
aaron <aaron.wolf@gmail.com>
2021-02-15 16:19:03 +0200

    homepage hero image and header style

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -9,6 +9,15 @@ const Homepage = () => {
                 <h1>Wolf Pack Developers</h1>
                 <h3>Creators of Beer Spot</h3>
             </div>
+            <div className="team left">
+                <h1>Our Team</h1>
+            </div>
+            <div className="blog right">
+                <h1>Blog</h1>
+            </div>
+            <div className="contact left">
+                <h1>Contact us</h1>
+            </div>
         </div>
     )
 }
