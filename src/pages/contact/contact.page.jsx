fb2681b01961a642a0b40363663cec8a41c492b2
Micah King <king.micah.b@gmail.com>
2021-03-04 12:54:25 +0200

    Reformatted contact page. Does not use a rich text editor because of issues with email service

--- src/pages/contact/contact.page.jsx
+++ src/pages/contact/contact.page.jsx
@@ -2,6 +2,11 @@ import emailjs from 'emailjs-com';
 import React, {useState} from 'react';
 
 import './contact.style.scss';
+import FormInput from './../../components/form-input/form-input.component';
+import FormArea from './../../components/form-input/form-area.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
+import {Editor} from '@tinymce/tinymce-react';
+import blog from '../../blog';
 
 const Contact = () => {
 
@@ -13,7 +18,7 @@ const Contact = () => {
 
     const sendEmail = (e) => {
         e.preventDefault();
-    
+        
         emailjs.sendForm('service_iyo7j7l', 'template_ky1oume', e.target, 'user_LKK2kTjNuqABfR8kaZ41y')
           .then((result) => {
               console.log(result.text);
@@ -26,20 +31,61 @@ const Contact = () => {
     const sentAlert = () => {
         <h1>Message Sent</h1>
     }
-
+    const [name, setName]=useState("");
+    const [email, setEmail]=useState("");
+    const [subject, setSubject]=useState("");
+    const [message, setMessage]=useState("");
     return (
         <div className="contact">
             {sent ? sentAlert : 
                 <form className="contact-form" onSubmit={sendEmail}>
-                <label>Name</label>
-                <input type="text" name="name" />
-                <label>Subject</label>
-                <input type="text" name="subject" />
-                <label>Email</label>
-                <input type="email" name="email" />
-                <label>Message</label>
-                <textarea name="message" />
-                <input type="submit" value="Send" />
+                <FormInput 
+                        name="name" 
+                        type='text' 
+                        value={name}
+                        handleChange={(event)=>{setName(event.target.value)}}
+                        label='Name'
+                        required
+                    />
+                <FormInput 
+                        name="email" 
+                        type='email' 
+                        value={email} 
+                        handleChange={(event)=>{setEmail(event.target.value)}}
+                        label='E-Mail'
+                        required
+                    />
+                <FormInput 
+                        name="subject" 
+                        type='Text' 
+                        value={subject}
+                        handleChange={(event)=>{setSubject(event.target.value)}}
+                        label='Subject'
+                        required
+                    />
+                    <FormArea name='message'
+                    type='text'
+                    value={message}
+                        handleChange={(event)=>{setMessage(event.target.value)}}
+                        label='Message'
+                        required
+                        />
+                {/* <Editor name="message"
+                        init={{selector: "textarea",
+                        menubar: false,
+                        plugins: "link image code",
+                        toolbar: 'undo redo | styleselect | forecolor | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link image | code'
+                        
+                    }}
+                        apikey={blog}
+                        onEditorChange={ (content, editor) => {
+                            setMessage(content);
+                            console.log(message);
+                          }}
+                        /> */}
+                        {/* <label>Message</label>
+                <textarea name="message" /> */}
+                <CustomButton type='submit'>Send</CustomButton>
               </form>
             }
         </div>
