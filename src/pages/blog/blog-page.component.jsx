7548a0a8be194da5d0c7edf0118e9582ca8ebb2e
Micah King <king.micah.b@gmail.com>
2021-03-15 11:01:09 +0200

    converted Blog-editor and blog-page to not use class, and instead use hooks

--- src/pages/blog/blog-page.component.jsx
+++ src/pages/blog/blog-page.component.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BlogPreview from './../../components/blog-preview/blog-preview.component';
 import BLOG_DATA from './../../blog.data.jsx';
 import './blog-page.styles.scss';
 
-class BlogPage extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state={
-            blogList:BLOG_DATA
-        }
-    }
-
-    render(){
-        const {blogList}=this.state;
-
-        return(
-            <div className='blog-page'>
-                {
-                    blogList.map(({id, ...otherBlogProps})=>(
-                        <BlogPreview key={id}{...otherBlogProps} />
-                    ))
-                }
-            </div>
-        );
-    }
+const BlogPage =()=>{
+    const [blogList, setBlogList]=useState(BLOG_DATA);
+    return(
+        <div className='blog-page'>
+            {
+                blogList.map(({id, ...otherBlogProps})=>(
+                    <BlogPreview key={id}{...otherBlogProps} />
+                ))
+            }
+        </div>
+    );
 }
+// class BlogPage extends React.Component{
+//     constructor(props){
+//         super(props);
+
+//         this.state={
+//             blogList:BLOG_DATA
+//         }
+//     }
+
+//     render(){
+//         const {blogList}=this.state;
+
+//         return(
+//             <div className='blog-page'>
+//                 {
+//                     blogList.map(({id, ...otherBlogProps})=>(
+//                         <BlogPreview key={id}{...otherBlogProps} />
+//                     ))
+//                 }
+//             </div>
+//         );
+//     }
+// }
 
 export default BlogPage;
